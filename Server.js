2e66e7fa28b07bf5b1cf9cec96b156e30127cc83
Blake Sanie <blake@sanie.com>
2018-12-09 19:33:34 -0800

    environmental port

--- Server.js
+++ Server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 //const Product = require("./Product.js");
 const app = express();
 const port = 3000;
-app.listen(port, () => console.log("server running on port " + port));
+app.listen(process.env.PORT || port, () =>
+  console.log("server running on port " + port)
+);
 /*
 mongoose.connect(
   "mongodb://bsanie:" +
