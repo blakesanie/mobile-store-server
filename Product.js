7a6ec8a64d5414ec41035466c4dd9e05dcdb410a
Blake Sanie <blake@sanie.com>
2018-12-09 16:55:25 -0800

    can not get products by category

--- Product.js
+++ Product.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  price: Number
+  minPrice: Number,
+  thumbUrl: String,
+  images: Array,
+  videos: Array,
+  versions: Array,
+  reviews: Array,
+  category: String
 });
 
 module.exports = mongoose.model("Product", schema);
--- Server.js
+++ Server.js
@@ -4,24 +4,45 @@ const keys = require("./keys");
 const Product = require("./Product.js");
 const app = express();
 const port = 3000;
+app.listen(port, () => console.log("server running on port " + port));
 
 mongoose.connect(
   "mongodb://bsanie:" +
     keys.adminPassword +
     "@mobilestore-shard-00-00-oardq.mongodb.net:27017,mobilestore-shard-00-01-oardq.mongodb.net:27017,mobilestore-shard-00-02-oardq.mongodb.net:27017/test?ssl=true&replicaSet=mobileStore-shard-0&authSource=admin&retryWrites=true",
-  { useNewUrlParser: true }
+  { useNewUrlParser: true, dbName: "storeIndex" }
 );
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => postProduct());
+app.get("/getproductsbycat/:category", (req, res) => {
+  var category = req.params.category;
+  //postProduct();
+  Product.find({ category: category })
+    .exec()
+    .then(docs => {
+      if (docs.length == 0) {
+        res.status(500).json({ error: "no products found" });
+      } else {
+        res.status(200).json(docs);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
 
 async function postProduct() {
   console.log(`Server listening on port ${port}!`);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
-    name: "second product",
-    price: 999
+    name: "Boosted Mini S",
+    minPrice: 600,
+    thumbUrl: "",
+    images: [],
+    videos: [],
+    versions: [],
+    reviews: [],
+    category: "gadgets"
   });
   product
     .save()
